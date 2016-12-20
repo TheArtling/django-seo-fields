f90ecca197431116b766b2e2489275d19efbbef3
Martin Brochhaus <mbrochh@gmail.com>
2016-12-20 17:15:59 +0800

    Removed django-compat & added home.html and dummy home view.

--- seo_fields/tests/urls.py
+++ seo_fields/tests/urls.py
@@ -1,10 +1,12 @@
 """URLs to run the tests."""
-from compat import include, url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
 ]
